99e25562fbf7618b3efd776c1178276985a37682
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2022-03-22 20:00:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,33 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //countriesContainer.style.opacity = 1;
+};
+const renderCountry = function (data, className = '') {
+  console.log(data);
+  //const languages = Object.values(data.languages);
+  //const currencies = Object.values(data.currencies);
+  const html = `<article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} mil people</p>
+        <p class="country__row"><span>🗣️</span>${
+          data.languages[Object.keys(data.languages)[0]]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          data.currencies[Object.keys(data.currencies)[0]].name
+        }</p>
+      </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  //countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////
 //https://restcountries.com/v3.1/name/{name}
 /*
@@ -37,27 +63,7 @@ const getCountryData = function (country) {
 };
 getCountryData('portugal');
 getCountryData('usa');
-*/
 
-const renderCountry = function (data, className = '') {
-  //console.log(data.currencies);
-  const [currencies] = Object.values(data.currencies);
-  const [languages] = Object.values(data.languages);
-  const html = `<article class="country ${className}">
-    <img class="country__img" src="${data.flags.png}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} mil people</p>
-      <p class="country__row"><span>🗣️</span>${languages}</p>
-      <p class="country__row"><span>💰</span>${currencies.name}</p>
-    </div>
-  </article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
 const getCountryAndNeighbor = function (country) {
   //AJAX call country 1
   const request = new XMLHttpRequest(); //old school AJAX call
@@ -86,7 +92,7 @@ const getCountryAndNeighbor = function (country) {
     });
   });
 };
-
+*/
 //getCountryAndNeighbor('usa');
 
 /////// MODERN WAY TO MAKE AJAX CALLS /////////////////////
@@ -99,9 +105,31 @@ const request = new XMLHttpRequest(); //old school AJAX call
 //const request = fetch('https://restcountries.com/v3.1/name/portugal');
 //console.log(request);
 //need to call json to read the response, returns a promise, have to call then on it to get data.
+
 const getCountryData = function (country) {
+  //country 2
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      //country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      //catches any errors in the whole promise chain
+      console.error(`${err}💥💥💥💥`);
+      renderError(`Something went wrong 💥💥💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      //for something that always needs to happen no matter the result of the promise
+      countriesContainer.style.opacity = 1;
+    });
 };
-getCountryData('Saudi Arabia');
+btn.addEventListener('click', function () {
+  getCountryData('germany');
+});
+getCountryData('oqhefouqhfuie');
